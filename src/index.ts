1219fdb90ed6d8856a31b379839d6e32f6ebaa5b
The-FerryMan1 <mercedroxy@gmail.com>
2025-05-03 17:15:11 +0800

    latest commit

--- src/index.ts
+++ src/index.ts
@@ -48,7 +48,7 @@ app.route('/email-verification', emailSendRoute);
 app.route('/login', loginRoute);
 app.route('/verify', verifyRoute);
 app.route('/refresh', refreshRoute);
-app.route('/logout', logoutRoute);
+app.route('/auth/logout', logoutRoute);
 
 
 //public route
