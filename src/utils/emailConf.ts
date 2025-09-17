c7e1b2d457f0c65bb3714512417bb6b1340b6c55
Roxy Merced <163075952+The-FerryMan1@users.noreply.github.com>
2025-09-17 12:43:05 +0800

    Update emailConf.ts

--- src/utils/emailConf.ts
+++ src/utils/emailConf.ts
@@ -1,7 +1,8 @@
 import * as nodemailer from 'nodemailer';
 import "dotenv/config";
 const transport = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    post: 465,
     auth: {
         user: process.env.GOOGLE_APP_EMAIL!,
         pass: process.env.GOOGLE_APP_PASSWORD!,
